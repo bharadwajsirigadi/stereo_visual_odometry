c8aa43feb4d20a2f093b89d23fe2a283980b877a
bharadwajsirigadi <bharadwajsirigadi@gmail.com>
2023-11-05 20:53:27 -0500

    initalization using spin

--- src/scripts/svo.py
+++ src/scripts/svo.py
@@ -3,7 +3,7 @@
 import rospy
 import numpy as np
 import message_filters
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge
 from nav_msgs.msg import Odometry, Path
 import cv2
@@ -12,8 +12,10 @@ class StereoVisualOdometry:
     def __init__(self):
         rospy.init_node('stereo_visual_odometry_node', anonymous=True)
         self.bridge = CvBridge()
-        self.left_img_sub = rospy.Subscriber('/car_1/camera/left/image_raw', Image, self.left_img_callback)
-        self.right_img_sub = rospy.Subscriber('/car_1/camera/right/image_raw', Image, self.right_img_callback)
+        self.left_img_sub = rospy.Subscriber('/car_1/camera/left/image_raw/compressed', CompressedImage)
+        self.right_img_sub = rospy.Subscriber('/car_1/camera/right/image_raw/compressed', CompressedImage)
+        ts = message_filters.TimeSynchronizer([self.left_img_sub, self.right_img_sub], 10)
+        ts.registerCallback(self.callback)
         self.odom_pub = rospy.Publisher('/car_1/odom', Odometry, queue_size=10)
         self.focal_length = 477.0
         self.baseline = 70 # 0.07 m
@@ -35,15 +37,19 @@ class StereoVisualOdometry:
     def compute_odometry(self, prev_img, curr_img):
         pass
 
-    def run(self):
-        rate = rospy.Rate(10)
-        while not rospy.is_shutdown():
+    def callback(self, left_img_msg, right_img_msg):
+
+        return
+
+    # def run(self):
+    #     rate = rospy.Rate(10)
+    #     while not rospy.is_shutdown():
  
-            rate.sleep()
+    #         rate.sleep()
 
 if __name__ == '__main__':
     svo = StereoVisualOdometry()
     try:
-        svo.run()
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
